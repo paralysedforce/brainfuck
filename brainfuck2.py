ef5072dab9cef923284741b024faad825b24305a
Vyas Alwar <VyasAlwar2018@u.northwestern.edu>
2018-07-26 14:06:53 -0500

    Fixed ScanLoop Optimizations

--- brainfuck2.py
+++ brainfuck2.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 import time
 import sys
+import argparse
 
 debug = False
 MAXSIZE = 8192
@@ -69,15 +70,11 @@ class OpenToken(Token):
         self.match = None
 
     def find_match(self):
-        if self.match: 
-            return 
-
         layer = 0
         for i in range(code.index(self) + 1, len(code)):
             if isinstance(code[i], CloseToken):
                 if layer == 0:
-                    self.match = i
-                    break
+                    return i
                 else:
                     layer -= 1
             elif isinstance(code[i], OpenToken):
@@ -86,7 +83,8 @@ class OpenToken(Token):
     def run(self):
         global pc
         if not mem[ptr]:
-            self.find_match()
+            if not self.match:
+                self.match = self.find_match()
             pc = self.match
 
     def __repr__(self):
@@ -97,14 +95,11 @@ class CloseToken(Token):
         self.match = None
 
     def find_match(self):
-        if self.match:
-            return 
         layer = 0
         for i in range(code.index(self) - 1, -1, -1):
             if isinstance(code[i], OpenToken):
                 if layer == 0:
-                    self.match = i
-                    break
+                    return i
                 else:
                     layer -= 1
             elif isinstance(code[i], CloseToken):
@@ -113,7 +108,8 @@ class CloseToken(Token):
     def run(self):
         global pc 
         if mem[ptr]:
-            self.find_match()
+            if not self.match:
+                self.match = self.find_match()
             pc = self.match
             
     def __repr__(self):
@@ -125,21 +121,26 @@ class ClearToken(Token):
     def __repr__(self):
         return "CLR"
 
-class ScanToken(ValToken):
+class ScanToken(Token):
+    def __init__(self, direction, value):
+        self.direction = direction
+        self.value = value
+
     def run(self):
         global mem, ptr
 
-        if self.value == 'L':
+        if self.direction == 'L':
             while mem[ptr]:
-                ptr -= 1
+                ptr -= self.value
 
-        elif self.value == 'R':
-#            print('trapped')
+        elif self.direction == 'R':
             while mem[ptr]:
-                ptr += 1
+                ptr += self.value
 
     def __repr__(self):
-        return "SCN " + self.value
+        return "SCN {0}: {1}".format(self.direction, self.value)
+
+class CopyToken(Token):
 
 # Converting into the intermdiate representation
 def build_brainfuck(code_string):
@@ -157,31 +158,6 @@ def build_brainfuck(code_string):
 def optimize_brainfuck():
     accumulate_optimization()
     loop_optimizations()
-
-def loop_optimizations():
-    global code
-
-    i = 0
-    while i < len(code) - 2:
-        if (isinstance(code[i], OpenToken) and
-            isinstance(code[i + 2], CloseToken)):
-
-            found = False
-            if isinstance(code[i + 1], SubToken):
-                code[i] = ClearToken()
-                found = True
-#            elif isinstance(code[i + 1], RightToken):
-#                code[i] = ScanToken('R')
-#                found = True
-#            elif isinstance(code[i + 1], LeftToken):
-#                code[i] = ScanToken('L')
-#                found = True
-
-            if found:
-                code.pop(i+2)
-                code.pop(i+1)
-        i += 1
-
 
 def accumulate_optimization():
     global code
@@ -211,6 +187,53 @@ def accumulate_optimization():
 
     code = new_code
 
+def loop_optimizations():
+    global code
+
+    i = 0
+    while i < len(code) - 2:
+        if (isinstance(code[i], OpenToken) and
+            isinstance(code[i + 2], CloseToken)):
+
+            found = False
+            if isinstance(code[i + 1], SubToken):
+                code[i] = ClearToken()
+                found = True
+            elif isinstance(code[i + 1], RightToken):
+                code[i] = ScanToken('R', code[i + 1].value)
+                print('fuck')
+                found = True
+            elif isinstance(code[i + 1], LeftToken):
+                code[i] = ScanToken('L', code[i + 1].value)
+                found = True
+#
+            if found:
+                code.pop(i+2)
+                code.pop(i+1)
+        i += 1
+
+def copy_optimization():
+    global code
+
+    while i < len(code):
+        if (isintance(code[i], OpenToken)):
+            movement = net_movement(i)
+            if movement == 0 and \
+                    (isinstance(code[i+1]
+                
+        i += 1
+
+def net_movement(start_of_block):
+    global code
+    
+    net_right = 0
+    for i in range(start_of_block, code[start_of_block].find_match()):
+        if isinstance(code[i], RightToken):
+            net_right += code[i].value
+        if isinstance(code[i], LeftToken):
+            net_right -= code[i].value
+    return net_right
+
 
 
 def run_brainfuck():
@@ -226,9 +249,16 @@ def run_brainfuck():
         print("\nDone")
 
 
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help='location of a brainfuck source file')
+    parser.add_argument('-d', '--debug', help='debug mode', action='store_true')
+    return parser
+
 def main():
-    filename = sys.argv[1]
-    with open(filename) as f:
+    parser = build_parser()
+    args = parser.parse_args()
+    with open(args.filename) as f:
         cmd = f.read()
 
     build_brainfuck(cmd)
